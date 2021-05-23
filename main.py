8c48e4401549e56897cc154a7ac10e2f9dc34293
zen1337 <33397782+zen1337@users.noreply.github.com>
2021-05-23 13:01:57 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame, os, sys, random
+class Game():
+    def __init__(self):
+        pygame.init()
+
+        self.load_textures()
+        
+
+    def load_texture(self):
+        pass
+
+    def load_sounds(self):
+        pass
+
+    def game(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def refresh_screen(self):
+        pass
+
+    def end(self):
+        pass
